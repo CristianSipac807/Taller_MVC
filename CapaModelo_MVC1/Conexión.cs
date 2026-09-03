da424f6f20b2ea4fe7848cda12f534868b6c0207
CristianSipac807 <cdavids0807@gmail.com>
2026-09-02 20:04:05 -0600

    Subiendo modelo

---
+++ CapaModelo_MVC1/Conexión.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaModelo_MVC1
+{
+    class Conexión
+    {
+        public OdbcConnection conexion()
+        {
+            OdbcConnection conn = new OdbcConnection("Dsn=umg_didactica_capacitacion");
+            try
+            {
+                conn.Open();
+            }
+            catch (OdbcException)
+            {
+                Console.WriteLine("No conecto :(");
+            }
+            return conn;
+        }
+        public void desconexion(OdbcConnection conn) 
+        {
+            try
+            {
+                conn.Close();
+            }
+            catch (OdbcException)
+            {
+                Console.WriteLine("No se Desconecto : )");
+            }
+        }
+
+    }
+}
---
+++ CapaModelo_MVC1/Sentencias.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaModelo_MVC1
+{
+    public class Sentencias
+    {
+        Conexión conn = new Conexión();
+        public OdbcDataAdapter llenarTbl(string nombreTabla)
+        {
+            string sSQL = "Select * from " + nombreTabla + " ;";
+            OdbcDataAdapter daSentencias = new OdbcDataAdapter(sSQL, conn.conexion());
+            return daSentencias;
+        }
+    }
+}
