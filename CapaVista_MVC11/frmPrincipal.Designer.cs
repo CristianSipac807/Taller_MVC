edd0d0bf5909236cb928297059900a6350839c95
CristianSipac807 <cdavids0807@gmail.com>
2026-09-02 19:59:40 -0600

    Subiendo vista

---
+++ CapaVista_MVC11/frmPrincipal.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace CapaVista_MVC11
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvConsultaTabla = new System.Windows.Forms.DataGridView();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConsultaTabla)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvConsultaTabla
+            // 
+            this.dgvConsultaTabla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvConsultaTabla.Location = new System.Drawing.Point(40, 228);
+            this.dgvConsultaTabla.Name = "dgvConsultaTabla";
+            this.dgvConsultaTabla.RowHeadersWidth = 51;
+            this.dgvConsultaTabla.RowTemplate.Height = 24;
+            this.dgvConsultaTabla.Size = new System.Drawing.Size(727, 150);
+            this.dgvConsultaTabla.TabIndex = 0;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(267, 121);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(157, 54);
+            this.btnConsultar.TabIndex = 1;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(260, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Cristian David Sipac Ispache 9959-23-1567";
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.dgvConsultaTabla);
+            this.Name = "frmPrincipal";
+            this.Text = "frmPrincipal";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConsultaTabla)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvConsultaTabla;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ CapaVista_MVC11/frmPrincipal.cs
@@ -0,0 +1,35 @@
+﻿using CapaControlador_MVC1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaVista_MVC11
+{
+    public partial class frmPrincipal : Form
+    {
+        string nombreTabla = "tipo_ruta";
+        Controlador controlador = new Controlador ();
+        public frmPrincipal()
+        {
+            
+            InitializeComponent();
+        }
+
+        public void actualizarDataGridView()
+        {
+            DataTable dtVista = controlador.llenarDgv(nombreTabla);
+            dgvConsultaTabla.DataSource = dtVista;
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            actualizarDataGridView();
+        }
+    }
+}
